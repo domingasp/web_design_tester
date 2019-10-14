3bfab6d86f26de2ca30ccc29f3f07d05a63fb453
Domingas Pleckauskas <domingas.p@yahoo.com>
2019-10-14 21:07:48 +0100

    Added multiple input fields and linked them to affect the currently selected element.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var selectedElement = document.body
+var selectedElement = document.getElementsByClassName("div-main__div-output__header__menu__li__a--company")[0];
 
 function getEventTarget(e) {
     e = e || window.event;
@@ -9,10 +9,62 @@ function getEventTarget(e) {
 
 function applyChanges() {
     bgColor = document.getElementById("bgColorInput").value;
-    fgColor = document.getElementById("fgColorInput").value
+    fgColor = document.getElementById("fgColorInput").value;
+
+    bgHoverColor = document.getElementById("bgHoverColorInput").value;
+    fgHoverColor = document.getElementById("fgHoverColorInput").value;
+
+    bgFocusColor = document.getElementById("bgFocusColorInput").value;
+    fgFocusColor = document.getElementById("fgFocusColorInput").value;
+
+    textDecor = document.getElementById("textDecor").value;
+    textDecorHover = document.getElementById("textDecorHover").value;
+    textDecorFocus = document.getElementById("textDecorFocus").value;
+
+    focusOutlineColor = document.getElementById("focusOutlineColorInput").value;
+    focusOutlineWidth = document.getElementById("focusOutlineWidthInput").value + "rem";
+    focusOutlineBlur = document.getElementById("focusOutlineBlurInput").value + "rem";
 
     selectedElement.style.backgroundColor = bgColor;
     selectedElement.style.color = fgColor;
+    selectedElement.style.textDecoration = textDecor;
+
+    if (selectedElement.tagName == "BUTTON" || selectedElement.tagName == "A" ) {
+        elementToApplyFunctionsTo = selectedElement;
+
+        elementToApplyFunctionsTo.addEventListener("mouseover", function() {
+            if (this != document.activeElement) {
+                this.style.backgroundColor = bgHoverColor;
+                this.style.color = fgHoverColor;
+                this.style.textDecoration = textDecorHover;
+            }
+        });
+
+        elementToApplyFunctionsTo.addEventListener("mouseout", function() {
+            if (this != document.activeElement) {
+                this.style.backgroundColor = bgColor;
+                this.style.color = fgColor;
+                this.style.textDecoration = textDecor;
+            }
+        });
+
+        elementToApplyFunctionsTo.addEventListener("focus", function() {
+            this.style.backgroundColor = bgFocusColor;
+            this.style.color = fgFocusColor;
+            this.style.textDecoration = textDecorFocus;
+
+            this.style["boxShadow"] = "0 0 " + focusOutlineBlur + " " + focusOutlineWidth + " " + focusOutlineColor;
+            this.style["outline"] = "none";
+        });
+
+        elementToApplyFunctionsTo.addEventListener("blur", function() {
+            this.style.backgroundColor = bgColor;
+            this.style.color = fgColor;
+            this.style.textDecoration = textDecor;
+
+            this.style["boxShadow"] = "none";
+        });
+    }
 }
 
 function changeBgColorTextField(inputField) {
@@ -26,6 +78,46 @@ function changeBgColorTextField(inputField) {
 function changeFgColorTextField(inputField) {
     colorInput = document.getElementById("fgColorInput");
     colorTextInput = document.getElementById("fgColorTextInput");
+
+    colorInput.value = inputField.value;
+    colorTextInput.value = inputField.value;
+}
+
+function changeBgHoverColorTextField(inputField) {
+    colorInput = document.getElementById("bgHoverColorInput");
+    colorTextInput = document.getElementById("bgHoverColorTextInput");
+
+    colorInput.value = inputField.value;
+    colorTextInput.value = inputField.value;
+}
+
+function changeFgHoverColorTextField(inputField) {
+    colorInput = document.getElementById("fgHoverColorInput");
+    colorTextInput = document.getElementById("fgHoverColorTextInput");
+
+    colorInput.value = inputField.value;
+    colorTextInput.value = inputField.value;
+}
+
+function changeBgFocusColorTextField(inputField) {
+    colorInput = document.getElementById("bgFocusColorInput");
+    colorTextInput = document.getElementById("bgFocusColorTextInput");
+
+    colorInput.value = inputField.value;
+    colorTextInput.value = inputField.value;
+}
+
+function changeFgFocusColorTextField(inputField) {
+    colorInput = document.getElementById("fgFocusColorInput");
+    colorTextInput = document.getElementById("fgFocusColorTextInput");
+
+    colorInput.value = inputField.value;
+    colorTextInput.value = inputField.value;
+}
+
+function changeFocusOutlineColorTextField(inputField) {
+    colorInput = document.getElementById("focusOutlineColorInput");
+    colorTextInput = document.getElementById("focusOutlineColorTextInput");
 
     colorInput.value = inputField.value;
     colorTextInput.value = inputField.value;
