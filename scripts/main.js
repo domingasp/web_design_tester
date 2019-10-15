219bc3e0349a2fa40ac910e8776b544a86e33c11
Domingas Pleckauskas <domingas.p@yahoo.com>
2019-10-15 22:05:48 +0100

    Added styling for the edit parameters sections. Styling for header, labels and input fields.

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,15 @@ function getEventTarget(e) {
     e = e || window.event;
 
     selectedElement = e.target || e.srcElement;
-    document.getElementById("currentlySelectedP").innerHTML = "Currently Selected: " + selectedElement.id;
+    document.getElementById("currentlySelectedP").innerHTML = selectedElement.innerHTML;
+
+    lengthLimmit = '<span class = "div-main__div-input__p__span">Currently Selected:</span> "'.length + 20;
+    
+    if (document.getElementById("currentlySelectedP").innerHTML.length < lengthLimmit) {
+        document.getElementById("currentlySelectedP").innerHTML = '<span class = "div-main__div-input__p__span">Currently Selected:</span> "' + selectedElement.innerHTML + '"';
+    } else {
+        document.getElementById("currentlySelectedP").innerHTML = '<span class = "div-main__div-input__p__span">Currently Selected:</span> "' + selectedElement.innerHTML.substring(0, 20) + '"...';
+    }
 }
 
 function applyChanges() {
